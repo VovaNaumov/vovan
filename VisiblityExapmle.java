11837035cd8f50d306212e27939412b6bbec0f79
VovaNaumov <gastto12345@gmail.com>
2015-11-09 18:55:05 +0200

    bo1

---
+++ VisiblityExapmle.java
@@ -0,0 +1,17 @@
+package visiblity;
+
+public class VisiblityExapmle {
+	
+	public int publicInt;
+	
+	int defaultInt;
+	
+	protected int protectedInt;
+	
+	private int privateInt;
+	
+	public void somthing() {
+		
+	}
+
+}
